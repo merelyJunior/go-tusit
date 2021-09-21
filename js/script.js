22644404c5bc422d69ef6c0c1219cfc8c0e03580
Vladimir <67648997+merelyJunior@users.noreply.github.com>
2021-09-22 02:00:09 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  //Popup window--------------------------------------
+  $(".show-popup").magnificPopup({
+    type: "inline",
+    removalDelay: 500, //delay removal by X to allow out-animation
+    callbacks: {
+      beforeOpen: function () {
+        this.st.mainClass = this.st.el.attr("data-effect");
+      },
+    },
+    midClick: true, // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
+});
